6c5b5f3d9ef1a4aea8be71f2cdd9e60200b726f3
YasminSLobo <125031166+YasminLobo@users.noreply.github.com>
2025-03-09 22:41:03 -0300

    obstaculo

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import pygame, sys, random
 from pygame.math import Vector2
 import time
+import os
 
 NIGHT_GREEN = (0, 50, 0)
 BLOOD_RED = (139, 0, 0)
@@ -12,7 +13,24 @@ GOLD = (255, 215, 0)
 
 cell_size = 30
 cell_number = 20
+import os
 
+# Carrega a imagem do obstáculo
+obstaculo_image_path = os.path.join(os.path.dirname(__file__), 'img', 'obstaculo.png')
+obstaculo_image = pygame.image.load(obstaculo_image_path)
+obstaculo_image = pygame.transform.scale(obstaculo_image, (cell_size, cell_size))  # Redimensiona a imagem para o tamanho do obstáculo
+# Função para gerar uma posição aleatória para os obstáculos
+def gera_pos_aleatoria(obstaculo_pos):
+    x = random.randint(0, cell_number - 1) * cell_size
+    y = random.randint(0, cell_number - 1) * cell_size
+
+    if (x, y) in obstaculo_pos:
+        return gera_pos_aleatoria(obstaculo_pos)
+
+    return x, y
+obstaculo_pos = [gera_pos_aleatoria([]) for _ in range(5)]  # Gera 5 obstáculos aleatórios
+def colisao(pos1, pos2):
+    return pos1 == pos2
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
@@ -158,6 +176,9 @@ class MAIN:
         self.objective_start_time = 0
         self.define_level_goals()
         self.increase_speed()
+        self.obstaculo_pos = [gera_pos_aleatoria([]) for _ in range(5)]  # Gera 5 obstáculos aleatórios
+
+    # Outras funções da classe MAIN...
 
     def define_level_goals(self):
         if self.level == 1:
@@ -168,16 +189,24 @@ class MAIN:
             self.apples_to_win = 20
         elif self.level == 4:
             self.apples_to_win = 25
-
+        if self.level == 1:
+            self.apples_to_win = 10
+        elif self.level == 2:
+            self.apples_to_win = 15
+        elif self.level == 3:
+            self.apples_to_win = 20
+        elif self.level == 4:
+            self.apples_to_win = 25
+    
     def increase_speed(self):
         self.speed = max(50, self.speed - 15)
         print(f"Snake speed increased to: {self.speed}")
-
+    
     def next_level(self):
         self.level += 1
         if self.level > 4:
             self.level = 1  # Wrap around to level 1 if we exceed the number of levels
-
+    
         self.current_background_color = self.background_colors[self.level - 1]
         self.apples_collected = 0
         self.fruit = FRUIT(self.snake.body)  # Passa o corpo da cobra atualizado
@@ -188,13 +217,12 @@ class MAIN:
         self.level_complete = False
         self.level_complete_timer = None
         pygame.time.set_timer(SCREEN_UPDATE, self.speed)
-
+    
         # Redirecionar para a tela inicial
         selected_level = main_menu(screen)
         self.level = selected_level
         self.define_level_goals()
         self.increase_speed()
-
     def update(self):
         if not self.game_over() and self.has_moved and not self.show_objective and not self.level_complete:
             self.snake.move_snake()
@@ -409,6 +437,202 @@ game_font = pygame.font.Font('Font/PoetsenOne-Regular.ttf', 25)
 SCREEN_UPDATE = pygame.USEREVENT
 
 selected_level = main_menu(screen)
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT(self.snake.body)
+        self.lives = 3
+        self.score = 0
+        self.has_moved = False
+        self.apples_collected = 0
+        self.apples_to_win = 5
+        self.xp = 0
+        self.level = 1
+        self.level_up = False
+        self.obstacle = None
+        self.obstacle_chance = 0.2
+        self.level_complete = False
+        self.speed = 150
+        self.background_colors = [BACKGROUND_COLOR, NIGHT_GREEN, BLOOD_RED, DARK_GRAY]
+        self.current_background_color = self.background_colors[0]
+        self.show_objective = True
+        self.objective_timer = 2000
+        self.objective_start_time = 0
+        self.define_level_goals()
+        self.increase_speed()
+        self.obstaculo_pos = [gera_pos_aleatoria([]) for _ in range(5)]  # Gera 5 obstáculos aleatórios
+
+    # Outras funções da classe MAIN...
+
+    def define_level_goals(self):
+        if self.level == 1:
+            self.apples_to_win = 10
+        elif self.level == 2:
+            self.apples_to_win = 15
+        elif self.level == 3:
+            self.apples_to_win = 20
+        elif self.level == 4:
+            self.apples_to_win = 25
+        if self.level == 1:
+            self.apples_to_win = 10
+        elif self.level == 2:
+            self.apples_to_win = 15
+        elif self.level == 3:
+            self.apples_to_win = 20
+        elif self.level == 4:
+            self.apples_to_win = 25
+    
+    def increase_speed(self):
+        self.speed = max(50, self.speed - 15)
+        print(f"Snake speed increased to: {self.speed}")
+    
+    def next_level(self):
+        self.level += 1
+        if self.level > 4:
+            self.level = 1  # Wrap around to level 1 if we exceed the number of levels
+    
+        self.current_background_color = self.background_colors[self.level - 1]
+        self.apples_collected = 0
+        self.fruit = FRUIT(self.snake.body)  # Passa o corpo da cobra atualizado
+        self.define_level_goals()
+        self.show_objective = True
+        self.objective_start_time = pygame.time.get_ticks()
+        self.increase_speed()  # Aumenta a velocidade da cobra
+        self.level_complete = False
+        self.level_complete_timer = None
+        pygame.time.set_timer(SCREEN_UPDATE, self.speed)
+    
+        # Redirecionar para a tela inicial
+        selected_level = main_menu(screen)
+        self.level = selected_level
+        self.define_level_goals()
+        self.increase_speed()
+    def update(self):
+        if not self.game_over() and self.has_moved and not self.show_objective and not self.level_complete:
+            self.snake.move_snake()
+            self.check_collision()
+            self.check_fail()
+
+    def draw_elements(self):
+        self.draw_grass()
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+        self.draw_score()
+        self.draw_lives()
+        if self.obstacle:
+            self.obstacle.draw_obstacle()
+        if self.show_objective:
+            self.draw_objective()
+
+    # Outras funções da classe MAIN...
+
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            self.snake.add_block()
+            self.snake.play_crunch_sound()
+            self.score += 1  # Aumenta a pontuação em 1 por maçã
+            self.apples_collected += 1
+
+            if self.apples_collected >= self.apples_to_win:
+                self.level_complete = True
+                self.level_complete_timer = pygame.time.get_ticks()
+            else:
+                self.fruit.randomize()
+
+
+        for block in self.snake.body[1:]:
+            if block == self.fruit.pos:
+                self.fruit.randomize()
+
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.lose_life()
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.lose_life()
+        if self.obstacle and self.snake.body[0] == self.obstacle.pos:
+            self.lose_life()
+
+    def lose_life(self):
+        self.lives -= 1
+        if not self.game_over():
+            self.reset_snake()
+
+    def game_over(self):
+        return self.lives <= 0
+
+    def draw_grass(self):
+        for row in range(cell_number):
+            for col in range(cell_number):
+                grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                if (row + col) % 2 == 0:
+                    pygame.draw.rect(screen, (30, 30, 30), grass_rect)
+                else:
+                    pygame.draw.rect(screen, (10, 10, 10), grass_rect)
+        border_rect = pygame.Rect(0, 0, cell_number * cell_size, cell_number * cell_size)
+        pygame.draw.rect(screen, BLOOD_RED, border_rect, 3)
+
+    def draw_score(self):
+        score_text = str(self.score)
+        score_surface = game_font.render(score_text, True, TEXT_COLOR)
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center=(score_x, score_y))
+        apple_rect = apple.get_rect(midright=(score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6, apple_rect.height)
+        pygame.draw.rect(screen, NIGHT_GREEN, bg_rect)
+        screen.blit(score_surface, score_rect)
+        screen.blit(apple, apple_rect)
+        pygame.draw.rect(screen, NIGHT_GREEN, bg_rect, 2)
+
+    def draw_lives(self):
+        lives_text = str(self.lives)
+        lives_surface = game_font.render(f"Lives: {lives_text} XP: {self.xp}", True, TEXT_COLOR)
+        lives_rect = lives_surface.get_rect(topleft=(10, 10))
+        screen.blit(lives_surface, lives_rect)
+
+    def draw_objective(self):
+        font = pygame.font.Font('Font/PoetsenOne-Regular.ttf', 40)
+        objective_text = f"Level {self.level}: Eat {self.apples_collected}/{self.apples_to_win} apples!"
+        objective_surface = font.render(objective_text, True, TEXT_COLOR)
+        objective_rect = objective_surface.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+        screen.blit(objective_surface, objective_rect)
+    
+    def draw_level_complete(self):
+        font = pygame.font.Font('Font/PoetsenOne-Regular.ttf', 60)
+        level_complete_text = f"Level {self.level} Complete!"
+        level_complete_surface = font.render(level_complete_text, True, WHITE)
+        level_complete_rect = level_complete_surface.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+        screen.blit(level_complete_surface, level_complete_rect)
+
+
+    def reset_game(self):
+        self.reset_snake()
+        self.fruit = FRUIT(self.snake.body)
+        self.lives = 3
+        self.score = 0
+        self.has_moved = False
+        self.apples_collected = 0
+        self.xp = 0
+        self.level = 1
+        self.level_up = False
+        self.obstacle = None
+        self.obstacle_chance = 0.2
+        self.speed = 150
+        self.define_level_goals()
+        pygame.time.set_timer(SCREEN_UPDATE, self.speed)
+        self.show_objective = True
+        self.objective_start_time = pygame.time.get_ticks()
+        self.current_background_color = self.background_colors[0]  # Reset background color
+        self.level_complete = False
+        self.level_complete_timer = None
+
+
+    def reset_snake(self):
+        self.snake.reset()
+        self.has_moved = False
+        self.snake.direction = Vector2(0, 0)
+
 main_game = MAIN()
 main_game.level = selected_level
 main_game.define_level_goals()
@@ -426,6 +650,13 @@ while True:
 
         screen.fill(main_game.current_background_color)  # Setting the background color
         main_game.draw_elements()
+
+        # Desenha os obstáculos e verifica colisão
+        for pos in obstaculo_pos:
+            if colisao((main_game.snake.body[0].x * cell_size, main_game.snake.body[0].y * cell_size), pos):
+                main_game.lose_life()
+            screen.blit(obstaculo_image, pos)
+
         if main_game.level_complete:
             main_game.draw_level_complete()
             if current_time - main_game.level_complete_timer > 2000:
